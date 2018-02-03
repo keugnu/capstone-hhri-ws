b41d04730897eb7887fec98defeb0ad75a470106
Stephen Brooks <sfb130030@utdallas.edu>
2018-02-02 18:38:13 -0600

    overhaul. compiles without error

---
+++ src/hbs2/include/Request.hpp
@@ -0,0 +1,54 @@
+// Request.hpp
+
+#ifndef __REQUEST_H_INCLUDED__
+#define __REQUEST_H_INCLUDED__
+
+#include <vector>
+
+class Request {
+    private:
+        char _id;
+        std::string _type;
+        bool _status;
+	int _size;
+	int _expected;
+    public:
+        std::vector<signed char> data;
+	
+        Request(const std::vector<signed char>, int);
+        char get_id();
+        std::string get_type();
+        bool get_status();
+	void set_status(bool);
+	int get_size();
+	int get_expected();
+};
+
+Request::Request(const std::vector<signed char> req, int num_bytes) {
+
+    switch (req[0]) {
+        case '0':
+            _type = "status";
+        case '1':
+            _type = "read";
+        case '2':
+            _type = "write";
+    }
+
+    _id = req[1];
+    _status = false;
+    _size = num_bytes;
+
+    for (int i = 0; i < _size; i++) { data.push_back(req[i + 2]); }
+
+}
+
+int Request::get_size() { return _size; }
+int Request::get_expected() { return _expected; }
+bool Request::get_status() { return _status; }
+char Request::get_id() { return _id; }
+std::string Request::get_type() { return _type; }
+void Request::set_status(bool status) { _status = status; }
+
+
+#endif
--- src/hbs2/src/nodes/service/i2c_mgr.cpp
+++ src/hbs2/src/nodes/service/i2c_mgr.cpp
@@ -1,64 +1,98 @@
 // System
 #include <sys/types.h>
 #include <string>
-#include <vector>
 #include <queue>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
 
 // ROS
 #include "ros/ros.h"
 #include "hbs2/i2c_bus.h"
 
+// Custom
+#include "Request.hpp"
+
+
 int fd;
-std::string iic_dev = "/dev/i2c-1";
-std::queue work_queue;
+char const* iic_dev = "/dev/i2c-1";
+std::queue<Request> work_queue, completed_queue;
 
 
-bool write_req(const std::vector<signed char>* req, const std::vector<signed char>* res) {
+bool write_req(Request* job) {
+    if (fd = open(iic_dev, O_RDWR) < 0) { return false; }
+    else {
+	char to_write[job->get_size() + 1] = {0};
+	to_write[0] = job->get_id();
+	for (int i = 1; i < job->data.size(); i++) {
+	    to_write[i] = job->data.at(i);
+	}
 
+	ioctl(fd, I2C_SLAVE, to_write[0]);
+
+	if (write(fd, to_write, job->get_size()) != job->get_size()) { return false; }
+    }
+
+    job->set_status(true);
     return true;
 }
 
-bool read_req(const std::vector<signed char>* req, const std::vector<signed char>* res) {
+bool read_req(Request* job) {
+    if (fd = open(iic_dev, O_RDWR) < 0) { return false; }
+    else {
+	char is_read[job->get_expected()] = {0};
+        is_read[0] = job->get_id();
+        for (int i = 1; i < job->data.size(); i++) {
+            is_read[i] = job->data.at(i);
+        }
 
+        ioctl(fd, I2C_SLAVE, is_read[0]);
+
+	if (read(fd, is_read, job->get_expected()) != job->get_expected()) { return false; }
+
+    }
+
+    job->set_status(true);	
     return true;
 }
 
 bool handle_req(hbs2::i2c_bus::Request &req, hbs2::i2c_bus::Response &res) {
-    if (req.request[0] == 0) { 
-        if (read_req(&req.request, &res.data)) {
-	    ROS_INFO("Read request to I2C bus (address: %d) succeeded.", req.request[1]);
-	    res.success = true;
-	}
-        else {
-	    ROS_INFO("Read request to I2C bus (address: %d) failed.", req.request[1]);
-	    res.success = false;
-	}
-    }
+    Request request = Request(req.request, req.size);
 
-    else if (req.request[0] == 1) {
-        if (write_req(&req.request, &res.data)) {
-	    ROS_INFO("Write request to I2C bus (address: %d) succeeded.", req.request[1]);
-	    res.success = true;
-	}
-        else { 
-	    ROS_INFO("Write request to I2C bus (address: %d) failed.", req.request[1]);
-	    res.success = false;
-	}
-    }
-
-    else { 
-        ROS_INFO("Request to I2C bus (type: %d) is not valid.", req.request[0]);
+    if (request.get_type() == "status") {
 	res.success = false;
+	for (int i = 0; i < completed_queue.size(); i++) {
+	    Request job = completed_queue.front();
+	    if (job.get_id() == req.request[1]) { res.success = true; break; }
+	    else { completed_queue.pop(); completed_queue.push(job); }    
+	}
+	completed_queue.pop();
+	return true;
     }
+    
+    if (!work_queue.empty()) {
 
+	// TODO sort on priority
+
+	Request job = work_queue.front();
+	work_queue.pop();
+	if (job.get_type() == "read") { 
+	    if (read_req(&job)) { completed_queue.push(job); }
+	    else { work_queue.push(job); }
+	}
+	else if (job.get_type() == "write") { 
+	    if (write_req(&job)) { completed_queue.push(job); }
+	    else { work_queue.push(job); }
+
+    	}
+    }
     return true;
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "i2c_bus_mgr_srv");
     ros::NodeHandle n;
-
-    // TODO implement priority queue
 
     ros::ServiceServer srv = n.advertiseService("i2c_srv", handle_req);
     ros::spin();        
