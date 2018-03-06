c8f5f312b8e6bd8d4f49ce4f5afe3d1bef078fcd
Stephen Brooks <sfb130030@utdallas.edu>
2018-03-05 18:06:15 -0600

    successful service call with mcp9808

--- src/hbs2/src/nodes/hardware/MCP9808.cpp
+++ src/hbs2/src/nodes/hardware/MCP9808.cpp
@@ -12,6 +12,7 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <vector>
+#include <iostream>
 
 #include "ros/ros.h"
 #include "hbs2/i2c_bus.h"
@@ -31,31 +32,25 @@ bool write_init(ros::ServiceClient &client, hbs2::i2c_bus &srv) {
 	    return true;    
     }
 
-    ROS_ERROR("Failed to call service i2c_srv");
+    ROS_ERROR("Failed to call service i2c_srv.");
     return false;
 }
 
 // Read temperature from register
 float read_temp(ros::ServiceClient &client, hbs2::i2c_bus &srv) {
-    srv.request.request.resize(3);
-    srv.request.request = {0x02, 0x18, 0x05};
-    srv.request.size = 3;
-
-    if (client.call(srv)) {
-	srv.request.request.resize(4);
-	srv.request.request = {0x01, 0x18, 0x00, 0x00};
-	srv.request.size = 4;
+    srv.request.request.resize(4);
+    srv.request.request = {0x01, 0x18, 0x05, 0x00};
+    srv.request.size = 4;
 	
-	if (client.call(srv)) {
-	    std::vector<signed char> data(srv.response.data);
-	    int temp = (data[0] & 0x1F) * 256 + data[1];
-	    if (temp > 4095) { temp -= 8192; }
-	    float fTemp = (temp * 0.0625) * 1.8 + 32;
+    if (client.call(srv)) {
+	std::vector<signed char> data(srv.response.data);
+	int temp = (srv.response.data.at(0) & 0x1F) * 256 + srv.response.data.at(1);
+	if (temp > 4095) { temp -= 8192; }
 
-	    return fTemp;
-	}
+	float fTemp = (temp * 0.0625) * 1.8 + 32;
+	return fTemp;
     }
-    ROS_ERROR("Failed to call service i2c_srv");
+    ROS_ERROR("Failed to call service i2c_srv.");
     return -1;
 }
 
@@ -67,11 +62,10 @@ int main(int argc, char **argv) {
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<hbs2::i2c_bus>("i2c_srv");
     hbs2::i2c_bus srv;
-
+    ROS_INFO("ROS Node [mcp9808] has started.");
     if (write_init(client, srv)) {
 	float temperature = read_temp(client, srv);
 	ROS_INFO("Temperature in Fahrenheit: %.2f\n", temperature);
-    }
-
+}
     return 0;	
 }
--- src/hbs2/src/nodes/service/i2c_mgr.cpp
+++ src/hbs2/src/nodes/service/i2c_mgr.cpp
@@ -15,13 +15,16 @@
 #include "Request.hpp"
 
 
-int fd;
-char const* iic_dev = "/dev/i2c-1";
+static int fd;
+static char const* iic_dev = "/dev/i2c-1";
 std::queue<Request> work_queue, completed_queue;
 
-
 bool write_req(Request* job) {
-    if (fd = open(iic_dev, O_RDWR) < 0) { return false; }
+    ROS_INFO("A write request has been made for device %u", job->get_id());
+    if ((fd = open(iic_dev, O_RDWR)) < 0) { 
+	ROS_ERROR("Cannot open I2C bus."); 
+	return false;
+    }
     else {
 	char to_write[job->data.size()] = {0};
 	char dev_addr = job->get_id();
@@ -29,42 +32,56 @@ bool write_req(Request* job) {
 	    to_write[i] = job->data.at(i);
 	}
 
-	ioctl(fd, I2C_SLAVE, dev_addr);
+	ioctl(fd, I2C_SLAVE, dev_addr); 
 
-	if (write(fd, to_write, job->data.size()) != job->data.size()) { return false; }
+	if (write(fd, to_write, job->data.size()) != job->data.size()) {
+	    ROS_ERROR("Failed to write to I2C bus.");
+	    return false;
+	} else {
+	    job->set_status(true);
+	    return true;
+	}
     }
-
-    job->set_status(true);
-    return true;
 }
 
 bool read_req(Request* job) {
-    if (fd = open(iic_dev, O_RDWR) < 0) { return false; }
+    ROS_INFO("A read request has been made for device %u", job->get_id());
+    if ((fd = open(iic_dev, O_RDWR)) < 0) {
+	ROS_ERROR("Failed to open I2C bus.");
+	return false;
+    }
     else {
 	char is_read[job->data.size()] = {0};
+	char read_reg[job->data.size()] = {0};
         char dev_addr = job->get_id();
+
         for (int i = 0; i < job->data.size(); i++) {
             is_read[i] = job->data.at(i);
         }
 
         ioctl(fd, I2C_SLAVE, dev_addr);
 
-	if (read(fd, is_read, job->data.size()) != job->data.size()) { 
+	write(fd, is_read, 1);
+	usleep(70000);
+
+	if (read(fd, read_reg, job->data.size()) != job->data.size()) { 
+	    ROS_ERROR("Failed to read from I2C bus.");
 	    return false; 
-	} else {
+	}
+	else {
 	    job->data.resize(job->data.size());
-    	    memcpy(&job->data[0], &is_read[0], job->data.size() * sizeof(char));
+    	    memcpy(&job->data[0], &read_reg[0], job->data.size() * sizeof(char));
 	    job->set_status(true);
 	    return true;
 	}
-
     }
 }
 
 bool handle_req(hbs2::i2c_bus::Request &req, hbs2::i2c_bus::Response &res) {
     Request request = Request(req.request, req.size);
-
+    
     if (request.get_type() == "status") {
+	ROS_INFO("A request has been made for the status of device %u", req.request[1]);
 	res.success = false;
 	for (int i = 0; i < completed_queue.size(); i++) {
 	    Request job = completed_queue.front();
@@ -83,11 +100,15 @@ bool handle_req(hbs2::i2c_bus::Request &req, hbs2::i2c_bus::Response &res) {
 	Request job = work_queue.front();
 	work_queue.pop();
 	if (job.get_type() == "read") { 
-	    if (read_req(&job)) { completed_queue.push(job); res.data = job.data; }
+	    if (read_req(&job)) { 
+		completed_queue.push(job); 
+		res.data = job.data;
+		res.success = true;
+	    }
 	    else { work_queue.push(job); }
 	}
 	else if (job.get_type() == "write") { 
-	    if (write_req(&job)) { completed_queue.push(job); }
+	    if (write_req(&job)) { completed_queue.push(job); res.success = true; }
 	    else { work_queue.push(job); }
 
     	}
@@ -100,6 +121,7 @@ int main(int argc, char **argv) {
     ros::NodeHandle n;
 
     ros::ServiceServer srv = n.advertiseService("i2c_srv", handle_req);
-    ros::spin();        
+    ROS_INFO("ROS I2c Bus Manager has started.");
+    ros::spin();
     return 0;
 }
