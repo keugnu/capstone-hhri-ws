98e8793356f3e071f789b8d6d9e81072fe49620b
Stephen Brooks <sfb130030@utdallas.edu>
2018-03-24 18:34:07 -0500

    return success

--- src/hbs2/src/nodes/service/tts_srv.cpp
+++ src/hbs2/src/nodes/service/tts_srv.cpp
@@ -17,15 +17,15 @@ bool handle_req(hbs2::tts::Request &req, hbs2::tts::Response &res)
     char buf[150];
     //std::string buf = "echo " + req.text + " | " + "festival --tts";
     int str_len = sprintf(buf, "/bin/bash -c 'echo \"%s\" \u007C festival --tts'", req.text.c_str());
-    if (system(buf) != 0)
-    {
-    ROS_ERROR("System call to festival failed.");
-    res.success = false;
+    if (system(buf) != 0) {
+        ROS_ERROR("System call to festival failed.");
+        res.success = false;
+        return false;
     }
-    else 
-    {
-    res.success = true;
-    ROS_INFO("System call to festival succeeded.");
+    else {
+        res.success = true;
+        ROS_INFO("System call to festival succeeded.");
+        return true;
     }  
 }
 
