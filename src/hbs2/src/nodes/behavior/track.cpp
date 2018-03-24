f3bc0c7cb147742e69a63c3837dd9f72b5c5a0fd
Stephen Brooks <sfb130030@utdallas.edu>
2018-03-24 16:38:39 -0500

    Track with servo

--- src/hbs2/src/nodes/behavior/track.cpp
+++ src/hbs2/src/nodes/behavior/track.cpp
@@ -1,26 +1,89 @@
+// System
 #include <stdio.h>
 #include <unistd.h>
-#include <linux/i2c-dev.h>
 #include <vector>
 #include <iostream>
 
+// ROS
 #include "ros/ros.h"
 #include "std_msgs/UInt16MultiArray.h"
+#include "hbs2/servo.h"
+
+
+ros::NodeHandlePtr n = NULL;
+int last_position = 90;
 
 void track_callback(const std_msgs::UInt16MultiArray::ConstPtr& msg) {
-    int i = 0;
-    for(std::vector<uint16_t>::const_iterator it = msg->data.begin(); it != msg->data.end(); ++it) {
-        ROS_INFO("IR sensor %i: %umm", (i+1), *it);
-        i++;
+    ros::ServiceClient servo_client = n->serviceClient<hbs2::servo>("servo_srv");
+    hbs2::servo srv_servo;
+
+    int index = 0;
+
+    for (int i = 1; i < msg->data.size(); i++) {
+        if (msg->data.at(i) < msg->data.at(index))
+            index = i;
+    }
+    
+
+    ROS_INFO("Last position: %i", last_position);
+    switch(index) {
+        case 1: {
+            ROS_INFO("Object is right of center.");
+            if (last_position < 140) {
+                srv_servo.request.command = 2;
+                srv_servo.request.speed = 10;
+                servo_client.call(srv_servo);
+                srv_servo.request.command = 1;
+                srv_servo.request.position = last_position + 10;
+                last_position += 10;
+                servo_client.call(srv_servo);
+            }
+            break;
+        }
+        case 0: {
+            ROS_INFO("Object is centered.");
+            int diff = 0;
+            if (last_position == 90) { break; }
+            else { 
+                diff = 90 - last_position;
+                if (diff > 0) {
+                    srv_servo.request.command = 1;
+                    srv_servo.request.position = last_position + 10;
+                    last_position += 10;
+                }
+                else {
+                    srv_servo.request.command = 1;
+                    srv_servo.request.position = last_position - 10;
+                    last_position -= 10;
+                }
+                servo_client.call(srv_servo);
+            }
+            break;
+        }
+        case 2: {
+            ROS_INFO("Object is left of center.");
+            if (last_position > 40) {
+                srv_servo.request.command = 2;
+                srv_servo.request.speed = 10;
+                servo_client.call(srv_servo);            
+                srv_servo.request.command = 1;
+                srv_servo.request.position = last_position - 10;
+                last_position -= 10;
+                servo_client.call(srv_servo); 
+            }
+            break;
+        }
+        default:
+            break;
     }
 
-//    ROS_INFO("Distance: %umm", msg->data);
 }
+
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "track");
-    ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("tpc_track", 5, track_callback);
+    n = ros::NodeHandlePtr(new ros::NodeHandle);
+    ros::Subscriber sub = n->subscribe("tpc_track", 5, track_callback);
 
     ros::spin();
 
