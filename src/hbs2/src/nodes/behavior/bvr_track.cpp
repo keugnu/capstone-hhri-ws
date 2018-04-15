be021127ff89c278d4f30ddd36c70dfe67b122f5
Stephen Brooks <sfb130030@utdallas.edu>
2018-04-15 18:02:12 -0500

    reorder case

--- src/hbs2/src/nodes/behavior/bvr_track.cpp
+++ src/hbs2/src/nodes/behavior/bvr_track.cpp
@@ -12,14 +12,14 @@ const int _SERVO_SPEED_ = 50;
 const int _SERVO_POS_CHANGE_ = 20;
 
 ros::NodeHandlePtr n = NULL;
-int last_position = 90;
+int last_position = 90; // starting position at initialization is 90 degrees
 
 /*  Function: track_callback
     desc:   Callback function for the IR sensor topic. Sends requests to the maestro service to move servos.
     inputs:
         &msg:   Array containing the current message on the IR sensor topic.
     outputs:
-        None
+        servo_client.call: sends a request to the maestro service
 */
 void track_callback(const std_msgs::UInt16MultiArray::ConstPtr& msg) {
     ros::ServiceClient servo_client = n->serviceClient<hbs2::servo>("servo_srv");
@@ -27,27 +27,13 @@ void track_callback(const std_msgs::UInt16MultiArray::ConstPtr& msg) {
 
     int index = 0;
 
+    // search through the topic queue and find the minimum
     for (int i = 1; i < msg->data.size(); i++) {
         if (msg->data.at(i) < msg->data.at(index))
             index = i;
     }
-    
 
-    ROS_INFO("Last position: %i", last_position);
     switch(index) {
-        case 1: {
-            ROS_INFO("Object is right of center.");
-            if (last_position < 140) {
-                srv_servo.request.command = 2;
-                srv_servo.request.speed = _SERVO_SPEED_;
-                servo_client.call(srv_servo);
-                srv_servo.request.command = 1;
-                srv_servo.request.position = last_position + _SERVO_POS_CHANGE_;
-                last_position += _SERVO_POS_CHANGE_;
-                servo_client.call(srv_servo);
-            }
-            break;
-        }
         case 0: {
             ROS_INFO("Object is centered.");
             int diff = 0;
@@ -68,9 +54,24 @@ void track_callback(const std_msgs::UInt16MultiArray::ConstPtr& msg) {
             }
             break;
         }
+        case 1: {
+            ROS_INFO("Object is right of center.");
+            if (last_position < 140) {
+                // Make sure that the servo speed is set properly before sending the position request
+                srv_servo.request.command = 2;
+                srv_servo.request.speed = _SERVO_SPEED_;
+                servo_client.call(srv_servo);
+                srv_servo.request.command = 1;
+                srv_servo.request.position = last_position + _SERVO_POS_CHANGE_;
+                last_position += _SERVO_POS_CHANGE_;
+                servo_client.call(srv_servo);
+            }
+            break;
+        }
         case 2: {
             ROS_INFO("Object is left of center.");
             if (last_position > 40) {
+                // Make sure that the servo speed is set properly before sending the position request
                 srv_servo.request.command = 2;
                 srv_servo.request.speed = _SERVO_SPEED_;
                 servo_client.call(srv_servo);            
