2a537b62fe0a3b2e20816e3f75b19266bc76c179
Stephen Brooks <sfb130030@utdallas.edu>
2018-04-17 17:45:48 -0500

    changing syntax

--- src/hbs2/src/nodes/service/tts_srv.cpp
+++ src/hbs2/src/nodes/service/tts_srv.cpp
@@ -23,7 +23,7 @@ bool handle_req(hbs2::tts::Request &req, hbs2::tts::Response &res)
 {
     char buf[150];
     int str_len = sprintf(buf, "/bin/bash -c 'echo \"%s\" \u007C festival --tts'", req.text.c_str());
-    if (!system(buf)) {
+    if (system(buf) != 0) {
         ROS_ERROR("System call to festival failed.");
         return false;
     }
